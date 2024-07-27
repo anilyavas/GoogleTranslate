977c4bb1af0dd733604668f12aa398ac36c33402
anilyavas <anil.yavas99@gmail.com>
2024-07-27 14:03:57 +0300

    input and output translation ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
-import { Entypo, Feather, FontAwesome } from '@expo/vector-icons';
-import { Stack, Link } from 'expo-router';
+import { Entypo, Feather, FontAwesome, FontAwesome5, FontAwesome6 } from '@expo/vector-icons';
+import { Stack } from 'expo-router';
 import { useState } from 'react';
-import { View, StyleSheet, Text, Pressable, TextInput } from 'react-native';
+import { View, StyleSheet, Text, Pressable, TextInput, ScrollView } from 'react-native';
 
 export default function Home() {
-  const [input, setInput] = useState<string | undefined>();
-  const [output, setOutput] = useState<string | undefined>();
+  const [input, setInput] = useState<string>('');
+  const [output, setOutput] = useState<string>('');
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Stack.Screen options={{ title: 'Translate' }} />
       <View style={styles.languageContainer}>
         <Text style={styles.language}>English</Text>
@@ -27,7 +27,7 @@ export default function Home() {
             multiline
             maxLength={5000}
           />
-          <Feather name="arrow-right-circle" color="royalblue" size={24} />
+          <Feather name="arrow-right-circle" color="royalblue" size={18} />
         </View>
         <View
           style={{
@@ -35,12 +35,27 @@ export default function Home() {
             justifyContent: 'space-between',
             alignItems: 'center',
           }}>
-          <FontAwesome name="microphone" color="black" size={24} />
+          <FontAwesome name="microphone" color="dimgrey" size={18} />
           <Text style={{ fontWeight: 'bold', color: 'grey' }}>{input?.length}/1000</Text>
         </View>
       </View>
-      <View style={styles.outputContainer} />
-    </View>
+      {output && (
+        <View style={styles.outputContainer}>
+          <View>
+            <Text style={styles.output}>{output}</Text>
+            <View
+              style={{
+                justifyContent: 'space-between',
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <FontAwesome6 name="volume-high" size={18} color="dimgray" />
+              <FontAwesome5 name="copy" size={18} color="dimgray" />
+            </View>
+          </View>
+        </View>
+      )}
+    </ScrollView>
   );
 }
 const styles = StyleSheet.create({
@@ -67,6 +82,19 @@ const styles = StyleSheet.create({
     borderColor: 'gainsboro',
   },
   input: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: '400',
+    minHeight: 200,
+    paddingRight: 5,
+  },
+  outputContainer: {
+    padding: 20,
+    gap: 5,
+    borderBottomWidth: 1,
+    borderColor: 'gainsboro',
+  },
+  output: {
     flex: 1,
     fontSize: 16,
     fontWeight: '400',
